8ec3a060febfc8a0b4338a81c8c1c47818ee9b0b
Tilda Kampman <tilda.kampman@gmail.com>
2024-06-01 22:30:56 +0200

    remove text from aboutpage

--- frontend/event-app/src/views/AboutView.vue
+++ frontend/event-app/src/views/AboutView.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>
-      This is a simple event app that allows users to create and view events.
   </div>
 </template>
 
